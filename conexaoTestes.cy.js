3663a7bc9f735194e2b3a035c84489fb8e562106
Fernando Baroni <86305650+FHBaroni@users.noreply.github.com>
2022-11-07 11:08:47 -0300

    M3S08Exercicio5

---
+++ conexaoTestes.cy.js
@@ -0,0 +1,81 @@
+context('Teste Conexão', () => {
+  before(() => {
+    cy.visit('https://localhost:44360/CalcSimples.aspx');
+  });
+
+  it('Teste 1', () => {
+    cy.get('#txtN1').type(10);
+    cy.get('#txtN2').type(134);
+
+    cy.get('input').contains('Calcular').click();
+    cy.get('#lbResultado').then((resultado) => {
+      const resSoma = resultado.text();
+
+      // multiplicação
+      cy.get('#btnLimpar').click();
+      cy.get('#txtN1').type(97);
+      cy.get('#rbFuncao_1').click();
+      cy.get('#txtN2').type(70);
+      cy.get('input').contains('Calcular').click();
+      cy.get('#lbResultado').then((resultado) => {
+        const resSubtracao = resultado.text();
+
+        cy.visit('https://localhost:44360/ControleEntradasSaidas.aspx');
+        cy.get('#txtV1').type(resSoma);
+        cy.get('#txtV2').type(resSubtracao);
+      });
+    });
+
+    cy.get('#txtN1').type('pássaros');
+    cy.get('#txtN2').type('peixes');
+    cy.get('input').contains('Enviar').click();
+  });
+  it.only('Teste 2', () => {
+    // multiplicação
+    cy.get('#txtN1').type(10);
+    cy.get('#rbFuncao_2').click();
+    cy.get('#txtN2').type(100);
+    cy.get('input').contains('Calcular').click();
+    cy.get('#lbResultado').then((resultado) => {
+      const resMultplicacao = resultado.text();
+      //divisão
+      cy.get('#btnLimpar').click();
+      cy.get('#txtN1').type(90);
+      cy.get('#rbFuncao_3').click();
+      cy.get('#txtN2').type(3);
+      cy.get('input').contains('Calcular').click();
+      cy.get('#lbResultado').then((resultado) => {
+        const resDivisao = resultado.text();
+        //soma
+        cy.get('#btnLimpar').click();
+        cy.get('#txtN1').type(125);
+        cy.get('#txtN2').type(25);
+        cy.get('input').contains('Calcular').click();
+        cy.get('#lbResultado').then((resultado) => {
+          const resSoma = resultado.text();
+          //subtração
+          cy.get('#btnLimpar').click();
+          cy.get('#txtN1').type(2);
+          cy.get('#rbFuncao_1').click();
+          cy.get('#txtN2').type(1);
+          cy.get('input').contains('Calcular').click();
+          cy.get('#lbResultado').then((resultado) => {
+            const resSubtracao = resultado.text();
+
+            cy.visit('https://localhost:44360/ControleEntradasSaidas.aspx');
+            cy.get('#txtV1').type(resMultplicacao);
+            cy.get('#txtV2').type(resDivisao);
+            cy.get('#txtV3').type(resSoma);
+            cy.get('#txtV4').type(resSubtracao);
+          });
+        });
+
+        cy.get('#txtN1').type('Ração');
+        cy.get('#txtN2').type('petiscos');
+        cy.get('#txtN3').type('Coleiras');
+        cy.get('#txtN4').type('Remédios');
+        cy.get('input').contains('Enviar').click();
+      });
+    });
+  });
+});
